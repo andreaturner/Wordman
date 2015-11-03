af4908ed6ebf89914ce059375ad4e2acf27ce2ea
Paige Israel <paige.israel@yahoo.com>
2015-11-03 10:28:59 -0800

    created w-circle-logo, inserted in header.php

--- pages/books1.php
+++ pages/books1.php
@@ -1,9 +1,9 @@
 <?php
-    $page_title = 'Sholama-Series';
+    $page_title = 'Series, Novels & Short Stories';
     include ('../shared/header.html');
 ?>
     <div class="content">
         <div class="book-series-summary">
             <div class="header">
-                Sholama &amp; Jamalok
+                Series Excerpts, Novels &amp; Short Stories
             </div>
