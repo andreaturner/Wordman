839c12eeadc2712d7881b83e1dacc551c006f3f2
Paige Israel <paige.israel@yahoo.com>
2015-12-04 02:00:28 -0800

    pages/books1.php, styles/styles.css

--- pages/books1.php
+++ pages/books1.php
@@ -1,10 +1,10 @@
 <?php
-    $page_title = 'Series Excerpts &amp; Novels ';
+    $page_title = 'Books ';
     include ('../shared/header.html');
 ?>
     <div class="content">
         <div class="keep-it-together">
-            <div class="header">
+            <div class="header-excerpts">
                     Series Excerpts 
             </div>
                  
@@ -12,7 +12,7 @@
                   <img src="../images/book-stack-239x300.jpg" width="239" height="300" alt="Stack of Books Image" /></a>
                 </div>
             
-                <div class="books-nav">
+                <div class="books-nav1">
                     <nav>
                         <ul>
                             <li><a href="iftmus-series.php">Iftmus</a></li>
@@ -33,41 +33,39 @@
                     <img src="../images/book-tree-242x300.jpg" width="242" height="300" alt="Book with Tree Image" /></a>
                 </div>
                 
-                <div class="books-nav">
+                <div class="books-nav2">
+                    <div class="small-header-novels">
+                        Novels
+                    </div>
                     <nav>
                         <ul>
-                            <li>Novels</li>
-                                <ul>
-                                    <li><a href="under-construction.php">Portraits of My Spirit In Passing</a></li>
-                                    <li><a href="under-construction.php">And There Isn't Any Magic Here Today</a></li>
-                                    <li><a href="under-construction.php">Paintin&#39; &#40;The Color of Change&#41;</a></li>
-                                </ul>
+                            <li><a href="under-construction.php">Portraits of My Spirit In Passing</a></li>
+                            <li><a href="under-construction.php">And There Isn't Any Magic Here Today</a></li>
+                            <li><a href="under-construction.php">Paintin&#39; &#40;The Color of Change&#41;</a></li>
                         </ul>
                     </nav>
                 </div>
                 
-                <div class="books-nav">
+                <div class="books-nav3">
+                    <div class="small-header-short-stories">
+                        Short Stories
+                    </div>
                     <nav>
                         <ul>
-                            <li>Short Stories</li>
-                                <ul>
-                                    <li><a href="under-construction.php">The Shortcut</a></li>
-                                    <li><a href="under-construction.php">Self Defense</a></li>
-                                    <li><a href="under-construction.php">Next Time the Wind Blows</a></li>
-                                </ul>
+                            <li><a href="under-construction.php">The Shortcut</a></li>
+                            <li><a href="under-construction.php">Self Defense</a></li>
+                            <li><a href="under-construction.php">Next Time the Wind Blows</a></li>
                         </ul>
                     </nav>
                 </div>
                 
-                <div class="books-nav">
+                <div class="books-nav3">
                     <nav>
                         <ul>
                             <li></li>
-                                <ul>
-                                    <li><a href="under-construction.php">The Last Step</a></li>
-                                    <li><a href="under-construction.php">Dreams</a></li>
-                                    <li><a href="under-construction.php">We Are Alone</a></li>
-                                </ul>
+                            <li><a href="under-construction.php">The Last Step</a></li>
+                            <li><a href="under-construction.php">Dreams</a></li>
+                            <li><a href="under-construction.php">We Are Alone</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -82,6 +80,18 @@
                 <div class="series-pic-books1">
                     <img src="../images/feather-quill-230x300.jpg" width="230" height="300" alt="Quill Pen Image" /></a>
                 </div>
+            </div>
+            <div class="books-nav1">
+                <div class="small-header-essays">
+                    Shards of a Broken Mirror
+                </div>
+                <!--<nav>
+                    <ul>
+                        <li><a href="under-construction.php">Songs</a></li>
+                        <li><a href="under-construction.php">Poems</a></li>
+                        <li><a href="under-construction.php">Thoughts</a></li>
+                    </ul>
+                </nav> -->
             </div>
             
             <div class="header">
@@ -99,7 +109,7 @@
                 
         </div> <!-- end keep-it-together div -->
     </div> <!-- end content div -->
-        
+       
         
 <?php
     include ('../shared/footer.html');            
