edf3b4af7cc01095f205ff5ceb7ef29d96a9073b
Paige Israel <paige.israel@yahoo.com>
2015-12-19 23:56:25 -0800

    added and resized images
    added and resized images for books2 page

---
+++ pages/books2.php
@@ -0,0 +1,17 @@
+<?php
+    $page_title = 'Books ';
+    include ('../shared/header.html');
+?>
+
+    <div class="content">
+        
+        
+        
+        
+        
+        
+    </div> <!-- end content div -->
+    
+<?php
+    include ('../shared/footer.html');            
+?>
