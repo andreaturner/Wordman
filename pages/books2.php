1f45665d8dca6889bfef0fcd615f31956104e470
Paige Israel <paige.israel@yahoo.com>
2015-12-21 15:42:49 -0800

    Working on Books2 page
    Have Books2 working, except for the plays-screenplays image not loading.
    Made additions to style sheets, re-sized and loaded images

--- pages/books2.php
+++ pages/books2.php
@@ -4,24 +4,86 @@
 ?>
 
     <div class="content">
-        <div class="keep-it-together">
-            <div class="text-left-books"> 
-            <div class="header">
-                <a href="novels.php"><span>N</span>ovels</a>
-            </div>
-            <div class="series-pic-books2">
-                <a href="novels.php"><img src="../images/novels-654x500.jpg" width="654" height="500" alt="Novels Image" /></a>
-            </div>
-        </div> <!-- end text-left-books -->
-                
-            <div class="text-right-books">
-                <div class="header">
-                    <a href="short-stories.php"><span>S</span>hort <span>S</span>tories
-                </div>
-                <div class="series-pic-books2-rt">
-                    <a href="short-stories.php"><img src="../images/short-stories-716x492.jpg" width="716" height="492" alt="Short Stories Image" /></a>
-                </div>
-            </div> <!-- end text-right-books -->
+        <div class="books-page">
+            <div class="keep-it-together">
+                <div class="text-left-books"> 
+                    <div class="header-left">
+                        <a href="novels.php"><span>N</span>ovels</a>
+                    </div>
+                    <div class="series-pic-books2">
+                        <a href="novels.php"><img src="../images/novels-654x500.jpg" width="654" height="500" alt="Novels Image" /></a>
+                    </div>
+                </div>  <!-- end text-left-books div -->
+                    
+                <div class="text-right-books">
+                    <div class="header-right">
+                        <a href="short-stories.php"><span>S</span>hort <span>S</span>tories
+                    </div>
+                    <div class="series-pic-books2-rt">
+                        <a href="short-stories.php"><img src="../images/short-stories-728x500.jpg" width="728" height="500" alt="Short Stories Image" /></a>
+                    </div>
+                </div> <!-- end text-right-books -->
+            </div>  <!-- end keep-it-together div -->
+        </div> <!-- end of books-page div -->
+        
+        <!-- start of second tier of subjects -->
+        <div class="books-page">
+            <div class="keep-it-together">
+                <div class="text-left-books"> 
+                    <div class="header-left-essays">
+                        <a href="essays-and-collections.php"><span>E</span>ssays and <span>C</span>ollections</a>
+                    </div>
+                    <div class="series-pic-books2">
+                        <a href="essays-and-collections.php"><img src="../images/essays-and-collections-687x500.jpg" width="687" height="500" alt="Essays and Collections Image" /></a>
+                    </div>
+                </div>  <!-- end text-left-books-essays div -->
+                    
+                <div class="text-right-books">
+                    <div class="header-right-children">
+                        <a href="children.php"><span>C</span>hildren&#39;s <span>B</span>ooks
+                    </div>
+                    <div class="series-pic-books2-rt">
+                        <a href="children.php"><img src="../images/children-624x500.jpg" width="624" height="500" alt="Children's Books Image" /></a>
+                    </div>
+                </div> <!-- end text-right-books-children -->
+            </div>  <!-- end keep-it-together div -->
+        </div> <!-- end of books-page div -->
+        
+        <!-- start of third tier of subjects -->
+        <div class="books-page">
+            <div class="keep-it-together">
+                <div class="text-left-books"> 
+                    <div class="header-left-music">
+                        <a href="music-albums.php"><span>M</span>usic, <span>A</span>lbums and <span>P</span>erformances</a>
+                    </div>
+                    <div class="series-pic-books2">
+                        <a href="music-albums.php"><img src="../images/music-albums-performances-626x498.jpg" width="626" height="500" alt="Music, Albums and Performances Image" /></a>
+                    </div>
+                </div>  <!-- end text-left-books-essays div -->
+                    
+                <div class="text-right-books">
+                    <div class="header-right-music">
+                        <a href="plays-screenplays.php"><span>P</span>lays and <span>S</span>creenplays
+                    </div>
+                    <div class="series-pic-books2-rt">
+                        <a href="plays-screenplays.php"<img src="../images/plays-screenplays-778x500.jpg" width="778" height="500" alt="Plays and Screenplays Image" /></a>
+                    </div>
+                </div> <!-- end text-right-books -->
+            </div>  <!-- end keep-it-together div -->
+        </div> <!-- end of books-page div -->
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    <!-- </div> <!-- end content div -->
   
   
   
@@ -29,18 +91,15 @@
   
   
   
-  
-  
-  
+            
     
-        </div> <!-- end keep-it-together div -->
+            
         
         
         
         
         
-        
-    </div> <!-- end content div -->
+    </div> 
     
 <?php
     include ('../shared/footer.html');            
