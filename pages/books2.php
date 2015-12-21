27044cdf1e12f34de0a3f63422519c987ba7d705
Paige Israel <paige.israel@yahoo.com>
2015-12-21 11:13:13 -0800

    Working on the page with the different types of content
    Working on getting Novels and Short Stories to present as I think I have
    them coded.  Wish me luck!

--- pages/books2.php
+++ pages/books2.php
@@ -4,6 +4,36 @@
 ?>
 
     <div class="content">
+        <div class="keep-it-together">
+            <div class="text-left-books"> 
+            <div class="header">
+                <a href="novels.php"><span>N</span>ovels</a>
+            </div>
+            <div class="series-pic-books2">
+                <a href="novels.php"><img src="../images/novels-654x500.jpg" width="654" height="500" alt="Novels Image" /></a>
+            </div>
+        </div> <!-- end text-left-books -->
+                
+            <div class="text-right-books">
+                <div class="header">
+                    <a href="short-stories.php"><span>S</span>hort <span>S</span>tories
+                </div>
+                <div class="series-pic-books2-rt">
+                    <a href="short-stories.php"><img src="../images/short-stories-716x492.jpg" width="716" height="492" alt="Short Stories Image" /></a>
+                </div>
+            </div> <!-- end text-right-books -->
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+        </div> <!-- end keep-it-together div -->
         
         
         
