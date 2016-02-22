f157baa39a282c30a75d3e6f8c062151fa6d3d86
Paige Israel <paige.israel@yahoo.com>
2016-02-21 23:10:05 -0800

    working on iftmus-chpt-1, changed styles slightly
    Still working on Chapter 1. Changed width to percentage. It is very,
    very, very long!

--- pages/iftmus-chpt-1.php
+++ pages/iftmus-chpt-1.php
@@ -95,6 +95,83 @@
                 night, thee shall go out and witness Kylor to all cat, carrying on the sacred trust of those that have done so before.&rdquo;</span>
             </div>
             
+            <div class="prologue-text">
+                Fa-tera and Ja-natia nodded, slowly in turn to each of the twelve cats. This semi-submissive bow was part of the ritual of a Teachers acceptance of the younger
+                cats worth, at this stage in their testing. <span class="italic">&ldquo;Each of thee is a reminder of what we have been, what we are and what we can be.  If thee pass this testing, let
+                all cat acknowledge with respect what thee have earned&#46;&#46;&#46;  and all that thee represent!&rdquo;</span>
+            </div>
+            
+            <div class="prologue-text">
+                The eye-moons of Re-nell peered through a tattered cloud that was swimming hesitantly across the darkened sky. The white light from the twin moons merely added more of a
+                gold wash to the flickering fire in the clearing. The reflections of both fire and moons shimmered in the eyes of each young cat. 
+            </div>
+            
+            <div class="prologue-text">
+                Fa-tera smiled inwardly. The old teacher knew, should the fire be dead and the moons in hiding, there would still be a youthful touch of spark in all their eyes. They were
+                all so anxiously alive; idealistic hearts burning with dream fire!
+            </div>
+            
+            <div class="prologue-text">
+                These twelve cats were on the threshold of becoming the newest generation of Shi-kauti, the keepers of Ky-lore in Iftmus. This night was the last sunfall before Di-chant
+                and the time of their testing.
+            </div>
+            
+            <div class="prologue-text">
+                Far in the distance a chilling howl, from a roving pack of Woodwars, sliced through the still night air. Fa-tera turned her gaze in the direction of the insidious cry.
+                She allowed herself a moment to wonder why a pack was down this far from the lo-hills?
+            </div>
+            
+            <div class="prologue-text">
+                Woodwars, though ferocious, were basically cowards unless they outnumbered their victims. In packs they could bring down a large, healthy Ti-breen cat. Alone or in pairs
+                they would only take on a cat, if it were weakened, wounded, or an unfortunate cub separated from its pride. More often they stalked small defenseless animals, such as
+                ra-katoos or stray mud-waumps. True to the viciousness of their breed they were also known to turn on the weaker of their own, when hunger prevailed for fail of the hunt.
+            </div>
+            
+            <div class="prologue-text">
+                As if hearing her thoughts, Ja-natia Cha-tain faced the breeze with a tilt of her head and then mindspoke. <span class="italic">&ldquo;I sense their hunger but do not
+                taste any other purpose in their movements. They are midst hunt and no threat to us.&rdquo;</span> Wisps of her long white fur lashed in the wind like graceful white smoke tendrils.
+            </div>
+            
+            <div class="prologue-text">
+                Another howl went up; this time it was the cry of success. Fa-tera shuddered and gave up a short prayer for the victim that loosed its spirit in the shedding of deathblood.
+                She nodded affirmation to her San-Tou and turned her attention back to the circle of cats, sighing, <span class="italic">&ldquo;This is life, the giving and taking of which belongs to the Spirit of Felis.&rdquo;</span>
+                With little else to add Fa-tera straightened her stance, <span class="italic">&ldquo;But thee are the conscience of our life. The heart and soul of Iftmus is Kylor&#46;&#46;&#46; the words!&rdquo;</span>
+                The old cat blinked and then allowed herself a moment away from formality with an uncharacteristic touch of sentiment. <span class="italic">&ldquo;Thee, my hearts, are my beloved children and in all of thee,
+                I am so very proud.&rdquo;</span> 
+            </div>
+            
+            <div class="prologue-text">
+                Ja-natia Cha-tain gave pause, with a mental nod, measuring and noting depth of heart of this cat. The eyes of all twelve cats squeezed closed, caught short by the unprecedented flattery from
+                their usually strict and formal teacher.
+            </div>
+            
+            <div class="prologue-text">
+                Chosen at birth from various families of cat in the three provinces of Iftmus, these young cats had all been brought to Pursong as kittens. The sanctuary school known as Seftora in the
+                Province of La-mor was an isolated glade, a sacred place that lay nestled between Gift-wa, the lake of silent waters and the river, Tears of Rensis. Here in Pursong, Seftora was the place
+                of learning, a sanctuary respected by all cat since the early days of the first ruling Sa-bata cats. Here in a vast area of beautiful trees and lush grass, dotted with yellow-green lich
+                moss, generations of cat were raised and taught by the Teacher to become Shi-kauti, keepers of Kylor. Here they learned the daily routines of living and survival, as well as being ingrained
+                with the histories, the laws, the lore and traditions of all cats of Iftmus. This was what bound all cat to one family. The sum of all cat experience was Kylor!
+            </div>
+            
+            <div class="prologue-text">
+                The place of testing, sat right on the Tears Of Rensis, river&#39;s edge. The poetic side of Fa-tera always reveled in the soothing song of the fast flowing water. The water whispered and laughed
+                over rocks. It cooed and swallowed itself in deep eddies. It was the teasing of tomorrow to the land that lay before it today. It was an echo of yesterday from the mountains and the heavens
+                from which it flowed.
+            </div>
+            
+            <div class="prologue-text">
+                The river, Tears of Rensis, swept in from the furthest boundaries of the three provinces and met in the middle of Iftmus at the bottomless falls of Fina’sha, the singular place where the tip of
+                each of the provinces touched. Something stirred in Fa-tera’s heart. She silently vowed to make a pilgrimage to those falls in the near future.
+            </div>
+            
+            <div class="prologue-text">
+                Nalas&#39;, the stone monoliths that surrounded the cats were a huddle of shadows from the past. The large stones were veined with greenstone and had been placed there by the Abyssinian cats
+                that once dwelt in La-mor. There was a peace to their presence, a tie with the past and the heritage of all cat. Only once an eight-season did they stir, as they had the so many sunwalks
+                before when Fa-tera had prepared for this testing.
+            </div>
+            
+            
+            
             
         
             
