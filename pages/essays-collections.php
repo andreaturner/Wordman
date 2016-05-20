97d6ea6782de90cc710507cf20ea35009601fa2a
Paige Israel <paige.israel@yahoo.com>
2016-05-20 04:58:58 -0700

    Created pages and cropped images
    Created plays-screenplays.php page
    Created music-albums-performances.php page
    Created essays-collections.php page
    
    Resized novels and children's images
    
    Cropped images for essays-collections, plays-screenplays,
    music-albums-performances

---
+++ pages/essays-collections.php
@@ -0,0 +1,19 @@
+<?php
+    $page_title = 'Essays & Collections';
+    include ('../shared/header.html');
+?>
+
+    <div class="books-page">
+        <div class="keep-it-together">
+            
+                <div class="header-novel">
+                    <a href="under-construction.php"><span>E</span>ssays & <span>C</span>ollections</a>
+                </div>
+                <div class="series-pic-books-novels">
+                    <a href="under-construction.php"><img src="../images/essays-collections-900x611.jpg" width="900" height="611" alt="Essays and Collections Image" /></a>
+                </div>
+             <!-- end text-left-books div -->
+            
+<?php
+    include ('../shared/footer.html');            
+?>
---
+++ pages/music-albums-performances.php
@@ -0,0 +1,19 @@
+<?php
+    $page_title = 'Music, Albums & Performances';
+    include ('../shared/header.html');
+?>
+
+    <div class="books-page">
+        <div class="keep-it-together">
+            
+                <div class="header-novel">
+                    <a href="under-construction.php"><span>M</span>usic, <span>A</span>lbums</a> & <span>P</span>erformances</a>
+                </div>
+                <div class="series-pic-books-novels">
+                    <a href="under-construction.php"><img src="../images/album-cover-900x908.jpg" width="900" height="908" alt="Album Cover Image" /></a>
+                </div>
+             <!-- end text-left-books div -->
+            
+<?php
+    include ('../shared/footer.html');            
+?>
