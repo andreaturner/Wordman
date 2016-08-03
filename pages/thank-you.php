aa09ff8976b9125443d232b669e4ca6628f2c83e
Paige Israel <paige.israel@yahoo.com>
2016-08-02 18:06:29 -0700

    Finished Thank You page
    1. Completed mark-up for thank-you.php
    2. Resized and cropped images.

--- pages/thank-you.php
+++ pages/thank-you.php
@@ -2,12 +2,29 @@
     $page_title = 'Thank You!';
     include ('../shared/header.html');
 ?>
-
-
-
-
-
-
+    <div class="content">
+        <br>
+        <div class="header">
+            Those who pursue their own curiosity<br>
+            are to be applauded.<br>
+            Those who translate such curiosity into a dream<br>
+            are to be admired.<br>
+        </div>
+        
+        <div class="image-border">
+            <img src="../images/thank-you-800x446.jpg" width="800px" height="446px" alt="Thank You Drawing" />
+        </div>
+        <br>
+        
+        <div class="header">
+            Thank you for your interest in my site.
+        </div>
+        
+        <div class="small-header vivaldi">
+            <span class="large">E</span>ric <span class="large">K</span>och
+        </div>
+        
+    </div>  <!-- [End content div] -->
 
 <?php
     include ('../shared/footer.html');            
