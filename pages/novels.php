0044d7e0ff4d1fc7527c01e2840846ae3c3e9181
Paige Israel <paige.israel@yahoo.com>
2016-10-03 04:15:29 -0700

    Added "Enter" to novels image
    Added the word "Enter" to the novels image.  Changed the file name and
    the link on the novels.php page.

--- pages/novels.php
+++ pages/novels.php
@@ -10,7 +10,7 @@
                     <a href="books-series.php"><span>N</span>ovels</a>
                 </div>
                 <div class="series-pic-books-novels">
-                    <a href="books-series.php"><img src="../images/novels-900x685.jpg" width="900" height="685" alt="Novels Image" /></a>
+                    <a href="books-series.php"><img src="../images/novels-enter-900x685.jpg" width="900" height="685" alt="Novels Image" /></a>
                 </div>
         </div> <!-- end keep-it-together div -->
     </div>  <!-- [end books-page] -->
