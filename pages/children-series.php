40ccd8408d05a6fa28bab503410af48961eeb64b
Paige Israel <paige.israel@yahoo.com>
2016-10-10 04:21:18 -0700

    Completed The Once Little Breeze entry
    1. Edited image for The Once Little Breeze
    2. Added text to the image.
    3. Edited css.

--- pages/children-series.php
+++ pages/children-series.php
@@ -44,47 +44,34 @@
 					 </div>
             </div>
              <br><br>
-            
                 
             <div class="book-series-summary-100">
-                <div class="small-header">
-                    The Ungodly Disciple
+                <div class="small-header-child">
+                    <a href="under-construction.php">The Once Little Breeze</a>
                 </div>
-                <div class="series-pic">
-                    <img src="../images/ungodly-disciple-264x400.jpg" width="264" height="400" alt="The Ungodly Disciple" />
+                <div class="series-pic-iftmus">
+                    <img src="../images/the-once-little-breeze-264x350.jpg" width="264" height="350" alt="The Once Little Breeze Image" />
                 </div>
-                <div class="series-text-adventure-list">
-                    Book I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;A Twist In The Mirror&rdquo;</span><br>
-                    Book II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;The Unexpected Tryst&rdquo;</span><br>
-                    Book III&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;The Final Portrait&rdquo;</span><br>
-                </div>
-                    <div class="poem">
-                        (3 - 17) &ldquo;No part in any one soul can discern<br> 
-                        &nbsp;&nbsp;a heart capable of gross betrayal;<br>
-                        Nor can we anticipate the callousness of the other soul,<br> 
-                        &nbsp;&nbsp;That has drunk itself so numb, as to allow it.<br>
-                        &nbsp;&nbsp;This is an evil that blindsides the trusting<br>
-                        and thrusts a knife into the heart of the innocent. <br>
-                        <br>
-                        (3 - 18)  &ldquo;As the heart bleeds some do not die.<br>
-                        &nbsp;&nbsp;There are a few that grow stronger<br>
-                        &nbsp;&nbsp;&nbsp;And a few that grow wiser.<br>
-                        &nbsp;&nbsp;Still... there are the &ldquo;too many&rdquo;<br>
-                        That remain twisted and pass that legacy on!&rdquo;
-                    </div>   
+                <div class="series-text-monster">
+						  <span class="italic">This story dedicated<br>
+						  To Lee and Dale Beasley,<br>
+						  Two mentors and friends<br>
+						  Whose faith and enthusiasm<br>
+						  Were sun and water to a seedling.<br>
+						  Thank you both for being<br>
+						  Kind friends and good teachers!
+						  </span>
+						  <br><br><br>
+					 </div>
+                    
             </div>
-            <div class="p-credit">
-                From the book of Nathanial,<br>
-                Scribe to the &ldquo;Ungodly Disciple&rdquo;<br>
-                Chapter three; Verse seventeen and eighteen
-            </div>
-            <br><br>
+            
         
             <div class="book-series-summary">
                 <div class="small-header">
                     <a href="sholama-series.php">The Ongoing Adventures of Sholama and Jamalok</a>
                 </div>
-                <div class="series-pic">
+                <div class="series-pic-iftmus">
                     <a href="sholama-series.php"><img src="../images/sholama-cover-463x300.jpg" width="463" height="300" alt="Sholama and Jamalok" /></a>
                 </div>
                 <div class="series-text-adventure-list">
