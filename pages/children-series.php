e88dc775f802107ff020d9a11ca489e72602ae57
Paige Israel <paige.israel@yahoo.com>
2016-10-10 03:55:41 -0700

    Added The Monster There Under My Bed
    1. Added image of The Monster There Under My Bed.
    2. Added text beside the image.
    3. Resized no-christmas image and added to page.

--- pages/children-series.php
+++ pages/children-series.php
@@ -9,46 +9,39 @@
                 <span>C</span>hildren's <span>B</span>ooks
             </div>
             <div class="small-header">
-                <a href="iftmus-series.php">Mr. B.E.R.</a>
+                <a href="under-construction.php">Mr. B.E.R.</a>
             </div>
             <div class="series-pic-iftmus">
-                <a href="iftmus-series.php"><img src="../images/no-christmas-247x300.jpg" width="247" height="300" alt="Mr. B.E.R. Image" /></a>
+                <a href="under-construction.php"><img src="../images/no-christmas-247x350.jpg" width="247" height="350" alt="Mr. B.E.R. Image" /></a>
             </div>
             <div class="series-text-adventure-list">
                 Book I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;The B.E.R. Who Couldn&#39;t Afford Christmas&rdquo;</a></span><br>
                 Book II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;Sir BER-A-LOT &#40;In Search of the Grumble Grump&#41;&rdquo;</a></span><br>
                 Book III&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;A B.E.R. Without A Home&rdquo;</a></span><br>
                 Book IV&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;Mr. B.E.R. &#40;A Three Dog Night&#41;&rdquo;</a></span><br>
-                Book V&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;Mr. B.E.R. &#40;To Find The Final Stitch&#41;&rdquo;</a></span><br>
+                Book V&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;Mr. B.E.R. &#40;To Find The Final Stitch&#41;&rdquo;</a></span><br>
             </div>
-            <div class="series-text-adventure">
-                <span class="large">T</span>hree unlikely Heroes from three worlds, make up the nine &ldquo;Nakostoma&rdquo; who are destined to be the final stand against the younger 
-                god of Chaos, &ldquo;Baushra&rdquo;. Baushra is stepping outside of the balance of life and is manifesting itself as a living god on all three worlds. 
-                Each of the group are fighting their own demons which only seems to make the battle more favored toward Baushra&#39;s goal. On all three 
-                worlds they fight to discover what may be the answer, win or lose, in the final confrontation between gods, legends and their own tormented souls.
-            </div>
-            </div>    
-                <div class="read-more">
-                    <a href="iftmus-series.php">Read more!</a>
-                </div>
+        </div>    
             
          <div class="book-series-summary-100">
                 <div class="small-header">
-                    <a href="alyssia-series.php">Old Alyssia</a>
+                    <a href="under-construction.php">The Monster There Under My Bed</a>
                 </div>
-                <div class="series-pic">
-                    <a href="alyssia-series.php"><img src="../images/alyssia-to-hunt-the-past-345x300.jpg" width="345" height="300" alt="Old Alyssia - To Hunt The Past Image" /></a>
+                <div class="series-pic-iftmus">
+                    <a href="under-construction.php"><img src="../images/the-monster-there-265x350.jpg" width="265" height="350" alt="The Monster There Under My Bed Image" /></a>
                 </div>
-                <div class="series-text-adventure-list">
-                    Book I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;To Hunt The Past&rdquo;</span><br>
-                    Book II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;To Face The Future&rdquo;</span><br>
-                    Book III&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large">&ldquo;To Accept The Present&rdquo;</span><br>
-                </div>
-                <div class="series-text-adventure">
-                    <span class="large">A</span>n old witch embraces her animals, nature and her few friends to combat the evil that the invading army of Myrsongot has wrought 
-                    upon them all. Even while trying to save them, she must battle the Ra-shing, the king&#39;s church and its soldiers the Rak-ta, a less palatable part of the
-                    world she thought she had left behind. 
-                </div>
+                <div class="series-text-monster">
+						  <span class="italic">This story dedicated to<br>
+						  Reese Koch, our first grandson,<br>
+						  Still early in the womb.<br>
+						  I wrote this Reese&#46;&#46;&#46; before you were conceived.<br>
+						  I dedicate it to you before you took your first breath.<br>
+						  You were and are, an anticipated joy, a gift&#46;&#46;&#46;<br>
+						  And I&#39;m sure you will be a match<br>
+						  For any monster there under your bed!
+						  </span>
+						  <br><br><br>
+					 </div>
             </div>
              <br><br>
             
